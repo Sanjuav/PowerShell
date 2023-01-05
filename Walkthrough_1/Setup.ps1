a5c5d4d6ebe6953c29a35613c7db14148bbd361d
sanju.av <L00170998@atu.ie>
2023-01-05 21:30:40 +0000

    Added PowerShell script onto the repository

---
+++ Walkthrough_1/Setup.ps1
@@ -0,0 +1,8 @@
+﻿#Check the existing version
+$PSVersionTable
+#Set an execution policy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+#Install nugget as a package provider
+Install-PackageProvider Nuget -MinimumVersion 2.8.5.201 -Force | Out-Null
+#Install the module
+Install-Module -Name PowershellGet -Force -AllowClobber
---
+++ Walkthrough_1/Verify PowerShell7.ps1
@@ -0,0 +1,2 @@
+﻿$I = 0
+$env:PSModulePath -split ';' |ForEach-Object {"[{0:NO}] {1}" -f $I++, $_}
